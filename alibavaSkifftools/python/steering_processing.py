de4087fe70a3848b374490b1b177327c0f89c1dd
Jordi Duarte Campderros <Jordi.Duarte.Campderros@cern.ch>
2017-07-05 10:20:08 +0200

    Include mechanism to create steering files, step dependent

--- alibavaSkifftools/python/steering_processing.py
+++ alibavaSkifftools/python/steering_processing.py
@@ -8,33 +8,110 @@ __maintainer__ = "Jordi Duarte-Campderros"
 __email__ = "jorge.duarte.campderros@cern.ch"
 __status__ = "Development"
 
-# The path to the steering files
-import os
-the_steering_file=os.path.join(os.path.dirname(__file__),'steering_files','01-ab_converter.xml')
-# -- FIXME 
+# The path to the steering files, after installation
+def get_template_path():
+    """Get the path where the steering files templates are
+    installed. Be careful, this functions highly depends of the
+    setup.py entry `package_data`
 
+    Return
+    ------
+    str: the local path folder where the steering files live
+    """
+    import os
+    return os.path.join(os.path.dirname(__file__),'steering_files')
+
+# Available arguments present in the steering file templates.
+# These arguments are dynamically changed on run time
+_ARGUMENTS = { 'ROOT_FILENAME': 'Name of the output root file created by the AIDA processor',
+        'RUN_NUMBER': 'The run number of the input file',
+        'ALIBAVA_INPUT_FILENAME': 'The input file name (ALIBAVA RAW data)',
+        'INPUT_FILENAME': 'The list of input file names (LCIO DATA)',
+        'OUTPUT_FILENAME': 'Name of the output LCIO file created by the LCIOOutputProcessor',
+        }
 
 # Dummy class to define what steering file is associated
 # with a given step and what arguments should be provided
 # to fill the template steering file
 class marlin_step(object):
     def __init__(self,step_name):
+        import os
         self.step_name = step_name
         self.token = '@'
+        self.steering_file = os.path.join(os.getcwd(),"{0}.xml".format(self.step_name))
+    
+    @property
+    def steering_file_content(self):
+        """Getter for the steering file template content.
+        If the data member still does not exist, if not, it will
+        create it by reading the content of the steering_file_template
+        file, otherwise it will return just the content of 
+        the data member
+
+        Return
+        ------
+        steering_file_content: str
+            the content of the steering file template
+        """
+        try:
+            return self._steering_file_content
+        except AttributeError:
+            with open(self.steering_file_template) as f:
+                self._steering_file_content =  f.read()
+            return self._steering_file_content
+    @steering_file_content.setter
+    def steering_file_content(self,newcontent):
+        """Update the content of the steering file
+        
+        Parameters
+        ----------
+        newcontent: str
+            the new content
+        """
+        self._steering_file_content = newcontent
+    
+    def required_arguments_description(self,argument):
+        """The description of the argument
+
+        Parameters
+        ----------
+        argument: str|int
+            the argument name or the argument index 
+
+        Raises
+        ------
+        KeyError
+            If the argument does not present in _ARGUMENTS 
+            static dict
+        IndexError:
+            If the argument index is higher than the number
+            of arguments present
+        """
+        if type(argument) == str:
+            return _ARGUMENTS[argument]
+        elif type(argument) == int:
+            return _ARGUMENTS[self.required_arguments(argument)]
+
+    def set_argument_value(self,argument,value):
+        """Substitute the argument by the introduced value
+        in the steering file
+
+        Parameters
+        ----------
+        argument: str
+            the name of the argument
+        value: Any python type
+            the value to be substituted
+        """
+        self.steering_file_content = self.steering_file_content.replace("{0}{1}{0}".format(self.token,argument),str(value))
+
 
 # Concrete implementations
 class pedestal_conversion(marlin_step):
-    def __init__(self,step_name):
-        super(pedestal_conversion).__init__(step_name)
-        self.steering_file = '01-ab_converter.xml'
-        self.required_arguments = ('ROOT_FILENAME','RUN_NUMBER', 'INPUT_FILENAME', 'OUTPUT_FILENAME')
-        self.required_arguments_description = (\
-                'Name of the output root file created by the AIDA processor',\
-                'The run number of the input file', 'The input file name (ALIBAVA RAW data)',\
-                'Name of the output LCIO file created by the LCIOOutputProcessor')
+    def __init__(self):
+        import os
+        super(pedestal_conversion,self).__init__('pedestal_conversion')
 
-# Static dict mapping the step with the template steering 
-# files and their available word-substitutions
-_wordsubs_list = {
-        'pedestal-conversion': '01-ab_converter.xml',
-        }
+        self.steering_file_template = os.path.join(get_template_path(),'01-ab_converter.xml')
+        self.required_arguments = ('ROOT_FILENAME','RUN_NUMBER', 'ALIBAVA_INPUT_FILENAME', 'OUTPUT_FILENAME')
+
