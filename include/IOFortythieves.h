a2452a0ef14b66dd42d0706163d1c735355f77b0
duartej <jordid@post.tau.ac.il>
2017-10-12 15:35:52 +0200

    Create the accessor class to the fortythieves ROOT files. Start to include the cluster analyzer into the open_sesame. Not available yet

---
+++ include/IOFortythieves.h
@@ -0,0 +1,97 @@
+// ***************************************************************
+// 
+// Manager to read the ROOT output for the fortythieve
+// exec. https://github.com/duartej/postproc-alibava/blob/master/bin/fortythieves.cc
+//
+// Entry point for the post-processing and cluster 
+// analysis of the ROOT created by fortythieves. 
+//
+// This class should be updated every time the ROOT 
+// trees structure or branch names from the fortythieves
+// are modified.
+//
+// ***************************************************************
+// Author: Jordi Duarte-Campderros (IFCA-CERN), 
+// jorge.duarte.campderros@cern.ch.
+// 
+// ***************************************************************
+#ifndef IOFORTYTHIEVES_H
+#define IOFORTYTHIEVES_H
+
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+
+// forward declaraations
+class TFile;
+class TTree;
+
+class IOFortythieves
+{
+    public:
+        IOFortythieves() = delete;
+        IOFortythieves(const std::string & filename,const int & chip);
+        ~IOFortythieves();
+    
+        // Prepare all the variables and fill the run header related vectors
+        // (noise, calibration, ...) ready to be used.
+        void initialize();
+
+        // Process the event entry
+        void process(const int & i);
+        // Get the total entries for the Events tree
+        inline int get_entries() const { return _entries; };
+
+        // Getter for the noise vector 
+        inline const std::vector<float> & noise() const { return _noise; };
+        inline const std::vector<float> & pedestal() const { return _pedestal; };
+        inline const std::vector<float> & calibration() const { return _calibration; };
+
+        // Getters for event-related 
+        inline int event_number() const { return _event_number; };
+        inline int run_number() const { return _run_number; };
+        inline float event_time() const { return _event_time; };
+        inline float temperature() const { return _temperature; };
+        inline const std::vector<float> & adc_data() const { return *_adc_data; }
+
+        // Accessor for plain branch variable
+        /*template <class T> 
+            const  T & get(const std::string & variable);
+        // Accessor for vector-like variable
+        template <class T>
+            const T & get(const std::string & variable, const int & index);
+        */
+
+    private:
+        // The current beetle
+        int _chip;
+
+        // The total number of entries in the Events tree
+        int _entries; 
+
+        // Data members
+        TFile * _file;
+        // A helper list to 
+        static std::list<std::string> _TREENAMES;
+        // The trees 
+        std::map<std::string,TTree*> _trees;
+
+        // The accessors attached to the trees
+        // -- the event time, temperature, run and event numbers
+        int _run_number;  // Check if it's worth it to use it instead of the one in the runHeader
+        int _event_number;
+        float _event_time;
+        float _temperature;
+        // -- the data (in ADC counts, pedestal and common mode extracted)
+        std::vector<float> * _adc_data;
+        // The noise, pedestal and calibration vectors
+        std::vector<float> _noise;
+        std::vector<float> _pedestal;
+        // Note that this vector provides the number of electrons
+        // which corresponds to a ADC (per channel)
+        std::vector<float> _calibration;
+
+};
+
+#endif
---
+++ src/IOFortythieves.cxx
@@ -0,0 +1,148 @@
+// ****************************************************
+// 
+// Manager to read the ROOT output for the fortythieve
+// exec. https://github.com/duartej/postproc-alibava/blob/master/bin/fortythieves.cc
+//
+// Entry point for the post-processing and cluster 
+// analysis of the ROOT created by fortythieves. 
+// 
+// XXX DOC IMPLEMENTATION
+//
+// ***********************************************
+// Author: Jordi Duarte-Campderros (IFCA-CERN), 
+// jorge.duarte.campderros@cern.ch.
+// 
+// ***********************************************
+#include "IOFortythieves.h"
+
+//#include "ALIBAVA.h"
+
+// ROOT headers
+#include "TFile.h"
+#include "TTree.h"
+
+// System headers
+//
+
+// The names of the trees
+std::list<std::string> IOFortythieves::_TREENAMES( { "runHeader", "postproc_runHeader", 
+        "Events", "postproc_Events" }) ;
+
+IOFortythieves::IOFortythieves(const std::string & filename,const int & chip) :
+    _chip(chip),
+    _entries(-1),
+    _file(nullptr),
+    _run_number(-1),
+    _event_number(-1),
+    _temperature(-1),
+    _adc_data(nullptr)
+{
+    // XXX Check the chip number chip < ALIBAVA::NOOFCHIPS
+    
+    // Create the root file
+    _file = TFile::Open(filename.c_str());
+    // XXX: Check validity of the pointer
+
+    // And obtain the relevant trees
+    for(auto & treename: _TREENAMES)
+    {
+        _trees[treename] = static_cast<TTree*>(_file->Get(treename.c_str()));
+        // Check entries
+        if(treename == "postproc_Events")
+        {
+            _entries = _trees[treename]->GetEntries();
+        }
+        // XXX: Check validity of the pointer
+        // De-activate branches (afterwards activate only the used ones)
+        _trees[treename]->SetBranchStatus("*",0);
+    }
+    // keep track of the branches which are going to be used
+    std::map<std::string,std::vector<std::string> > used_branches;
+    used_branches["Events"].push_back("eventTime");
+    used_branches["Events"].push_back("temperature");
+    used_branches["Events"].push_back("eventNumber");
+    used_branches["Events"].push_back("runNumber");
+    // Remember the ROOT file notation for the chip is chip+1 (it has beetle 1 and 2)
+    const std::string adc_data_str("postproc_data_beetle"+std::to_string(chip+1));
+    used_branches["postproc_Events"].push_back(adc_data_str);
+
+    // Read both trees at the same time
+    _trees["postproc_Events"]->AddFriend(_trees["Events"]);
+
+    // Activate the branches to be used
+    for(const auto & treename_vectorbr: used_branches)
+    {
+        for(const auto & branch_name: treename_vectorbr.second)
+        {
+            _trees[treename_vectorbr.first]->SetBranchStatus(std::string(branch_name+"*").c_str(),1);
+        }
+    }
+
+    // Attach the trees to the corresponding data-member
+    // -- TDC time, temperature, run and event numbers
+    _trees["Events"]->SetBranchAddress("eventTime",&_event_time);
+    _trees["Events"]->SetBranchAddress("temperature",&_temperature);
+    _trees["Events"]->SetBranchAddress("eventNumber",&_event_number);
+    _trees["Events"]->SetBranchAddress("runNumber",&_run_number);
+
+    // -- ADCs corrected by common noise and pedestals
+    _trees["postproc_Events"]->SetBranchAddress(adc_data_str.c_str(),&_adc_data);
+}
+
+IOFortythieves::~IOFortythieves()
+{
+    // delete and de-allocate memory
+    if(_file != nullptr)
+    {
+        _file->Close();
+        delete _file;
+        _file = nullptr;
+    }
+}
+
+void IOFortythieves::initialize()
+{
+    // Obtain the pedestal, noise and calibration from the postproc 
+    // runHeader
+    
+    // 1. Create the auxiliary vectors to set the branch
+    //    address to the runHeader
+    // Remember the ROOT file notation for the chip is chip+1 (it has beetle 1 and 2)
+    const std::string noise_str("noise_cmmd_beetle"+std::to_string(_chip+1));
+    const std::string pedes_str("pedestal_cmmd_beetle"+std::to_string(_chip+1));
+    const std::string calib_str("electronADC_beetle"+std::to_string(_chip+1));
+    // helper map 
+    std::map<std::string,std::vector<float>* > br_names = { {noise_str,nullptr}, 
+        {pedes_str,nullptr}, {calib_str,nullptr} };
+    for(auto & name_vector: br_names)
+    {
+        _trees["postproc_runHeader"]->SetBranchStatus(std::string(name_vector.first+"*").c_str(),1);
+        _trees["postproc_runHeader"]->SetBranchAddress(name_vector.first.c_str(),&(name_vector.second));
+    }
+
+    // 2. Get the first entry of the header and fill the related datamembers
+    _trees["postproc_runHeader"]->GetEntry(0);
+    
+    // 3. Pass the filled vectors to the data members
+    _noise = *(br_names[noise_str]);
+    _pedestal = *(br_names[pedes_str]);
+    _calibration = *(br_names[calib_str]);
+    
+    // 4. De-activate the tree again, not needed anymore
+    _trees["postproc_runHeader"]->ResetBranchAddresses();
+    _trees["postproc_runHeader"]->SetBranchStatus("*",0);
+    for(auto & br_vtr: br_names)
+    {
+        if( br_vtr.second != nullptr)
+        {
+            delete br_vtr.second;
+            br_vtr.second = nullptr;
+        }
+    }
+}
+
+void IOFortythieves::process(const int & i)
+{
+    // Recall that the Events treee is friend of this,
+    _trees["postproc_Events"]->GetEntry(i);
+}
